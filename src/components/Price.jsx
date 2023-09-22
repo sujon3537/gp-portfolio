48e2781cfbc871651b730cc6fcb05eb582e1148f
sujon3537 <119605414+sujon3537@users.noreply.github.com>
2023-09-23 01:43:24 +0600

    pricing

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -7,31 +7,31 @@ const Price = () => {
       <h2 className="text-[17px] font-semibold text-white mb-6">
         Freelance Rate
       </h2>
-      <div className="flex justify-between">
-        <div className="bg-[#2D2D3A] w-[250px] h-[500px]">
-          <h5 className="font-sm font-semibold text-white">Consultation</h5>
+      <div className="grid grid-cols-3 gap-x-6">
+        <div className="bg-[#2D2D3A] h-fit flex flex-col items-center gap-y-6 p-7">
+          <h5 className="font-semibold text-sm text-white">Consultation</h5>
           <span className="text-[32px] font-semibold text-primary">
             FREE<sup>*</sup>
           </span>
-          <ul>
-            <li>
-              <BsCheck />
+          <ul className="flex flex-col items-center gap-y-2">
+            <li className="text-textColor text-center text-sm">
+              <BsCheck className="text-primary text-lg inline-block mr-1" />
               Identify needs
             </li>
-            <li>
-              <BsCheck />
+            <li className="text-textColor text-center text-sm">
+              <BsCheck className="text-primary text-lg inline-block mr-1" />
               Βuild development strategy
             </li>
-            <li>
-              <BsCheck />
+            <li className="text-textColor text-center text-sm">
+              <BsCheck className="text-primary text-lg inline-block mr-1" />
               Analyze bottlenecks
             </li>
-            <li>
-              <BsCheck />
+            <li className="text-textColor text-center text-sm">
+              <BsCheck className="text-primary text-lg inline-block mr-1" />
               Gather requirements
             </li>
-            <li>
-              <BsCheck />
+            <li className="text-textColor text-center text-sm">
+              <BsCheck className="text-primary text-lg inline-block mr-1" />
               Suggest optimal solution
             </li>
           </ul>
@@ -42,8 +42,78 @@ const Price = () => {
             contact now
           </a>
         </div>
-        <div className="bg-[#2D2D3A] w-[250px] h-[500px]"></div>
-        <div className="bg-[#2D2D3A] w-[250px] h-[500px]"></div>
+        <div className="bg-[#2D2D3A] h-fit flex flex-col items-center gap-y-6 p-7">
+          <h5 className="font-semibold text-sm text-white">Small Projects</h5>
+          <span className="text-[#fafafc] text-[13px] font-extralight">
+            &#8364;
+            <span className="text-[32px] font-semibold text-primary mx-2">
+              59
+            </span>
+            hour
+          </span>
+          <ul className="flex flex-col items-center gap-y-2">
+            <li className="text-textColor text-center text-sm">
+              <BsCheck className="text-primary text-lg inline-block mr-1" />
+              Web Development
+            </li>
+            <li className="text-textColor text-center text-sm">
+              <BsCheck className="text-primary text-lg inline-block mr-1" />
+              Custom Plugins
+            </li>
+            <li className="text-textColor text-center text-sm">
+              <BsCheck className="text-primary text-lg inline-block mr-1" />
+              Debugging
+            </li>
+            <li className="text-textColor text-center text-sm">
+              <BsCheck className="text-primary text-lg inline-block mr-1" />
+              WordPress integration
+            </li>
+            <li className="text-textColor text-center text-sm">
+              <BsCheck className="text-primary text-lg inline-block mr-1" />
+              SEO optimization
+            </li>
+          </ul>
+          <a
+            href="#"
+            className="text-primary text-[12px] font-semibold uppercase hover:drop-shadow-[0_0_3px_rgba(250,250,252,0.4)]"
+          >
+            contact now
+          </a>
+        </div>
+        <div className="bg-[#2D2D3A] h-fit flex flex-col items-center gap-y-6 p-7">
+          <h5 className="font-semibold text-sm text-white">Long Term</h5>
+          <span className="text-[32px] font-semibold text-primary">
+            Contact
+          </span>
+          <ul className="flex flex-col items-center gap-y-2">
+            <li className="text-textColor text-center text-sm">
+              <BsCheck className="text-primary text-lg inline-block mr-1" />
+              Large Scale Projects
+            </li>
+            <li className="text-textColor text-center text-sm">
+              <BsCheck className="text-primary text-lg inline-block mr-1" />
+              Complete websites or eshops
+            </li>
+            <li className="text-textColor text-center text-sm">
+              <BsCheck className="text-primary text-lg inline-block mr-1" />
+              Ongoing Website Maintenance
+            </li>
+            <li className="text-textColor text-center text-sm">
+              <BsCheck className="text-primary text-lg inline-block mr-1" />
+              Hosting & Tech Support
+            </li>
+            <li className="text-textColor text-center text-sm">
+              <BsCheck className="text-primary text-lg inline-block mr-1" />
+              WordPress optimization
+            </li>
+          </ul>
+          <a
+            href="#"
+            className="text-primary text-[12px] font-semibold uppercase hover:drop-shadow-[0_0_3px_rgba(250,250,252,0.4)]"
+          >
+            contact now
+          </a>
+        </div>
       </div>
     </div>
   );
